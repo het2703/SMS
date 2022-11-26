c902b9a9ffd15e7bce22c4420fee3075d0982bc6
Atharv <atharvtelkar@gmail.com>
2022-11-26 16:23:54 +0530

    Updated the scene by labelling the text

--- src/main/java/main/Controller/HelloController.java
+++ src/main/java/main/Controller/HelloController.java
@@ -3,7 +3,13 @@ package main.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class HelloController {
-
+    @FXML
+    private Label welcomeText;
+    @FXML
+    public void onHelloButtonClick(){
+        welcomeText.setText("This is done by us");
+    }
 }
