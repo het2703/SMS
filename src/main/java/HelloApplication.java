1a78dfe10bfc5002cee7b355470aea37ef8b83f6
Atharv <atharvtelkar@gmail.com>
2022-12-06 00:14:55 +0530

    1.1

--- src/main/java/HelloApplication.java
+++ src/main/java/HelloApplication.java
@@ -8,9 +8,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("main/hello-view.fxml"));
-        stage.setTitle("Hello!");
-        stage.setScene(new Scene(root, 320, 240));
+
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+        stage.setTitle("Hello");
+        stage.setScene(new Scene(root));
         stage.show();
     }
 //YE ek team project he
