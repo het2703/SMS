86779005ef4db7969c13247d067c838cff15f801
Atharv <atharvtelkar@gmail.com>
2022-12-07 19:15:04 +0530

    1.3

---
+++ src/main/java/UI/Controller/DashboardController.java
@@ -0,0 +1,29 @@
+package UI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.io.StreamTokenizer;
+
+public class DashboardController {
+@FXML
+    void logoutnow(MouseEvent event) throws IOException{
+
+}
+    }
+
---
+++ src/main/java/UI/Controller/SignupController.java
@@ -0,0 +1,71 @@
+package UI.Controller;
+
+import java.awt.*;
+import java.io.IOException;
+import java.sql.SQLException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import sms.Users;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.EventObject;
+
+public class SignupController {
+    @FXML
+    private Button login11button;
+    @FXML
+    private TextField newname;
+@FXML
+private RadioButton Admin;
+@FXML
+private RadioButton Cashier;
+    @FXML
+    private PasswordField newpassword;
+
+    @FXML
+    private TextField newuserid;
+    @FXML
+    void login11now(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("fxml/login.fxml"));
+        Stage stage =(Stage) ((Node) event.getSource()).getScene().getWindow();
+        BorderPane pane = loader.load();
+        stage.getScene().setRoot(pane);
+        stage.show();
+    }
+    @FXML
+    void validatesignup(MouseEvent event) throws SQLException, ClassNotFoundException, IOException {
+        boolean a= checksignup();
+        if(a){
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("fxml/login.fxml"));
+            Stage stage =(Stage) ((Node) event.getSource()).getScene().getWindow();
+            BorderPane pane = loader.load();
+            stage.getScene().setRoot(pane);
+            stage.show();
+        }
+    }
+
+    @FXML
+    public boolean checksignup() throws SQLException, ClassNotFoundException {
+        int newid = Integer.parseInt(newuserid.getText());
+        String s="";
+        if(Admin.isSelected()){
+            s="ADMIN";
+        }
+        else if (Cashier.isSelected()) {
+        s="CASHIER";
+        }
+
+       return Users.createNewUser(newid,newname.getText(),s,newpassword.getText());
+    }
+
+}
