b57e840ea5c07619722b997a437f71a637da05a7
Atharv <atharvtelkar@gmail.com>
2023-04-26 02:05:19 +0530

    1.80003

--- src/main/java/UI/Controller/CustomerController.java
+++ src/main/java/UI/Controller/CustomerController.java
@@ -1,20 +1,35 @@
 package UI.Controller;
 
+import Database.DAO.CustomerDAO;
+import Database.DB;
 import UI.Elements.ConfirmBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import sms.Customer;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class CustomerController {
+import static Database.DB.size;
+
+public class CustomerController implements Initializable {
 
     @FXML
     private Button addcustomerbutton;
@@ -25,21 +40,24 @@ public class CustomerController {
     @FXML
     private Button dashboard1;
     @FXML
-    private TableView<?> tableview;
-    @FXML
-    private TableColumn<?, ?> gendercolumn;
+    private TableView<Customer> tableview;
+//    @FXML
+//    private TableColumn<Customer, String> gendercolumn;
 
     @FXML
-    private TableColumn<?, ?> idcolumn;
+    private TableColumn<Customer, Integer> idcolumn;
 
     @FXML
-    private TableColumn<?, ?> namecolumn;
+    private TableColumn<Customer, String> namecolumn;
 
     @FXML
-    private TableColumn<?, ?> phonenumbercolumn;
+    private TableColumn<Customer, Integer> phonenumbercolumn;
 
     @FXML
     private TextField searchtext;
+
+    @FXML
+    private TableView<Customer> customerview;
 
     @FXML
     void addcustomer(MouseEvent event) throws IOException {
@@ -72,5 +90,51 @@ public class CustomerController {
         BorderPane pane = loader.load();
         stage.getScene().setRoot(pane);
         stage.show();
+    }
+
+    private final ObservableList<Customer> datalist = FXCollections.observableArrayList();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        namecolumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("first_name"));
+        idcolumn.setCellValueFactory(new PropertyValueFactory<Customer, Integer>("customer_id"));
+        phonenumbercolumn.setCellValueFactory(new PropertyValueFactory<Customer, Integer>("phone_number"));
+     //   gendercolumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("gender"));
+
+
+        try {
+            String query = "select * from customer";
+            ResultSet data = DB.dbExecuteQuery(query);
+            assert data != null;
+            int size = size(data);
+            Customer[] cus = new Customer[size];
+            CustomerDAO.dataToArray(data,cus);
+            for (int i = 0; i < cus.length ; i++) {
+                datalist.add(i,cus[i]);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        FilteredList<Customer> filtereddata = new FilteredList<>(datalist, b -> true);
+        searchtext.textProperty().addListener((observableValue, oldValue, newValue) ->
+        {
+            filtereddata.setPredicate(person -> {
+                if (newValue == null && newValue.isEmpty()) {
+                    return true;
+                }
+                String lowerCaseFilter = newValue.toLowerCase();
+                if (person.getFname().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                    return true;
+                }
+                 else {
+                    return false;
+                }
+            });
+        });
+        SortedList<Customer> sorteddata = new SortedList<>(filtereddata);
+        sorteddata.comparatorProperty().bind(customerview.comparatorProperty());
+        customerview.setItems(sorteddata);
+
     }
 }
--- src/main/java/UI/Controller/StockController.java
+++ src/main/java/UI/Controller/StockController.java
@@ -1,21 +1,34 @@
 package UI.Controller;
 
+import Database.DAO.EmployeeDAO;
+import Database.DAO.ProductDAO;
+import Database.DB;
 import UI.Elements.ConfirmBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import sms.Employee;
 import sms.Products;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class StockController {
+import static Database.DB.size;
+
+public class StockController implements Initializable {
     @FXML
     private Button addbutton;
     @FXML
@@ -37,6 +50,20 @@ public class StockController {
     @FXML
     private Button logout;
 
+    @FXML
+    private TableColumn<Products, Integer> product_id;
+
+    @FXML
+    private TableColumn<Products, String> p_name;
+
+    @FXML
+    private TableColumn<Products,Integer> price;
+
+    @FXML
+    private TableColumn<Products,Integer> stock;
+
+    @FXML
+    private TableView<Products> product_view;
     @FXML
     void logoutnow(MouseEvent event) throws IOException {
         boolean b = ConfirmBox.displayAlert("Logout?","Confirm logout");
@@ -86,5 +113,50 @@ public class StockController {
         int price=Integer.parseInt(productprice.getText());
         int stock=Integer.parseInt(productquantity.getText());
         return Products. createNewProduct(id, pname, price, stock);
+    }
+
+    private final ObservableList<Products> datalist = FXCollections.observableArrayList();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        product_id.setCellValueFactory(new PropertyValueFactory<Products, Integer>("product_id"));
+        p_name.setCellValueFactory(new PropertyValueFactory<Products, String>("P_name"));
+        price.setCellValueFactory(new PropertyValueFactory<Products, Integer>("price"));
+        stock.setCellValueFactory(new PropertyValueFactory<Products, Integer>("stock"));
+
+        try {
+            String query = "select * from products";
+            ResultSet data = DB.dbExecuteQuery(query);
+            assert data != null;
+            int size = size(data);
+            Products[] p = new Products[size];
+            ProductDAO.dataToArray(data,p);
+            for (int i = 0; i < p.length ; i++) {
+                datalist.add(i,p[i]);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        FilteredList<Products> filtereddata = new FilteredList<>(datalist, b -> true);
+        productname.textProperty().addListener((observableValue, oldValue, newValue) ->
+        {
+            filtereddata.setPredicate(person -> {
+                if (newValue == null && newValue.isEmpty()) {
+                    return true;
+                }
+                String lowerCaseFilter = newValue.toLowerCase();
+                if (person.getName().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                    return true;
+                }
+                 else {
+                    return false;
+                }
+            });
+        });
+        SortedList<Products> sorteddata = new SortedList<>(filtereddata);
+        sorteddata.comparatorProperty().bind(product_view.comparatorProperty());
+        product_view.setItems(sorteddata);
+
     }
 }
